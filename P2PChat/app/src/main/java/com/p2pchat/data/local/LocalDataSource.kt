fb509e0469fdc7539ec60728f04552e075bbd758
ByteChester <mehmetalidokuyucu32@gmail.com>
2026-08-16 13:10:57 +0300

    Update LocalDataSource.kt

--- P2PChat/app/src/main/java/com/p2pchat/data/local/LocalDataSource.kt
+++ P2PChat/app/src/main/java/com/p2pchat/data/local/LocalDataSource.kt
@@ -9,6 +9,7 @@ import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.map
 import kotlinx.serialization.encodeToString
 import kotlinx.serialization.json.Json
+import kotlinx.coroutines.flow.first
 
 val Context.dataStore: DataStore<Preferences> by preferencesDataStore(name = "p2pchat_prefs")
 
